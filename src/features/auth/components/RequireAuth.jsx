9930bf12415990a74a6fb3a7b4203010a0beaa05
Samantha <samantica.dr@gmail.com>
2026-03-14 03:01:48 -0400

    Se creo para garantizar que solo los usuarios autenticados puedan acceder a ellos

---
+++ src/features/auth/components/RequireAuth.jsx
@@ -0,0 +1,14 @@
+﻿import { Navigate, useLocation } from 'react-router-dom'
+
+function RequireAuth({ children }) {
+  const location = useLocation()
+  const isLogged = localStorage.getItem('orchirdSession') === 'active'
+
+  if (!isLogged) {
+    return <Navigate to="/login" replace state={{ redirectTo: location.pathname }} />
+  }
+
+  return children
+}
+
+export default RequireAuth
